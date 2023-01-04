3de0b10886ad41f876262bce94191c693523c8e2
Don Campbell <timebox.designs@gmail.com>
2023-01-04 14:37:33 -0700

    add InboxScreen

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,7 +1,11 @@
 /* A simple redux store/actions/reducer implementation.
  * A true app would be more complex and separated into different files.
  */
-import { configureStore, createSlice } from "@reduxjs/toolkit";
+import {
+  configureStore,
+  createAsyncThunk,
+  createSlice,
+} from "@reduxjs/toolkit";
 
 /*
  * The initial state of our store when the app loads.
@@ -13,11 +17,30 @@ const defaultTasks = [
   { id: "3", title: "Something else", state: "TASK_INBOX" },
   { id: "4", title: "Something again", state: "TASK_INBOX" },
 ];
+
 const TaskBoxData = {
   tasks: defaultTasks,
   status: "idle",
   error: null,
 };
+
+/*
+ * Creates an asyncThunk to fetch tasks from a remote endpoint.
+ * You can read more about Redux Toolkit's thunks in the docs:
+ * https://redux-toolkit.js.org/api/createAsyncThunk
+ */
+export const fetchTasks = createAsyncThunk("todos/fetchTodos", async () => {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/todos?userId=1"
+  );
+  const data = await response.json();
+
+  return data.map((task) => ({
+    id: `${task.id}`,
+    title: task.title,
+    state: task.completed ? "TASK_ARCHIVED" : "TASK_INBOX",
+  }));
+});
 
 /*
  * The store is created here.
@@ -35,6 +58,29 @@ const TasksSlice = createSlice({
         state.tasks[task].state = newTaskState;
       }
     },
+  },
+  /*
+   * Extends the reducer for the async actions
+   * You can read more about it at https://redux-toolkit.js.org/api/createAsyncThunk
+   */
+  extraReducers(builder) {
+    builder
+      .addCase(fetchTasks.pending, (state) => {
+        state.status = "loading";
+        state.error = null;
+        state.tasks = [];
+      })
+      .addCase(fetchTasks.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.error = null;
+        // Add any fetched tasks to the array
+        state.tasks = action.payload;
+      })
+      .addCase(fetchTasks.rejected, (state) => {
+        state.status = "failed";
+        state.error = "Something went wrong";
+        state.tasks = [];
+      });
   },
 });
 
